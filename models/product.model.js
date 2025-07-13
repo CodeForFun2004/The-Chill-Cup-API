d4c39974800b7885a1f6d84eaa459635ebe5b3a1
Truong <lenhattruong295@gmail.com>
2025-07-14 00:54:17 +0700

    only test and fix

--- models/product.model.js
+++ models/product.model.js
@@ -1,22 +1,21 @@
 // Product.schema.ts
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    basePrice: Number,
-    image: String,
-    status: { type: String, enum: ['new', 'old'], default: 'new' },  // món mới ra mắt, món cũ để cập nhật cho các cửa hàng
-    rating: { type: Number, min: 0, max: 5 },
-    sizeOptions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Size' }],
-    toppingOptions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Topping' }],
-    storeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Store' },
-    categoryId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],  // 1 product có thể có 2 cate(trường hợp matcha/ new )
-    isBanned: {
-        type: Boolean,
-        default: false
-      },
-  });
-  
+  name: String,
+  description: String,
+  basePrice: Number,
+  image: String,
+  status: { type: String, enum: ["new", "old"], default: "new" }, // món mới ra mắt, món cũ để cập nhật cho các cửa hàng
+  rating: { type: Number, min: 0, max: 5 },
+  sizeOptions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Size" }],
+  toppingOptions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Topping" }],
+  storeId: { type: mongoose.Schema.Types.ObjectId, ref: "Store" },
+  categoryId: [{ type: mongoose.Schema.Types.ObjectId, ref: "Category" }], // 1 product có thể có 2 cate(trường hợp matcha/ new )
+  isBanned: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-  module.exports = mongoose.model('Product', productSchema);  
+module.exports = mongoose.model("Product", productSchema);
